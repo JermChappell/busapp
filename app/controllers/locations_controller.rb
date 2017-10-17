e1a02834b535ed1ab02594c436daf45354bcd66b
Jeremy Chappell <jeremy.chappell87@gmail.com>
2017-10-17 16:12:43 +0000

    setup bus api and only keep buses that are close to me

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,6 @@
 class LocationsController < ApplicationController
   before_action :set_location, only: [:show, :edit, :update, :destroy]
-
+  include LocationsHelper
   # GET /locations
   # GET /locations.json
   def index
@@ -10,6 +10,12 @@ class LocationsController < ApplicationController
   # GET /locations/1
   # GET /locations/1.json
   def show
+    source_url = http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetAllBus
+    
+    @buses = fetch_buses_from_api(source_url)
+    
+    @buses.select! do |bus|
+      is_nearby?(@location.latitude, @location.longitude, bus["LATITUDE"], bus["LONGITUDE"])
   end
 
   # GET /locations/new
